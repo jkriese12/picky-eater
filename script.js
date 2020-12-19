7ec921e310c56e473bbdcee318d8d7c6d39f9373
Jacob Kriese <jkriese12@gmail.com>
2020-12-19 12:50:12 -0700

    AJAX UPDATE

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-$(h1).text();
-//test
+console.log("Test");
+// API KEY : bead57aed9e1420dbed8834d45b0f26e
+var queryURL =
+  "https://api.spoonacular.com/recipes/76257/information?apiKey=bead57aed9e1420dbed8834d45b0f26e";
+
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
