4389894bc890079ee5f7519887c9d0ff1a02a23a
Aphrodite <72478102+onomatopoetica@users.noreply.github.com>
2020-12-21 20:12:02 -0700

    Added search bar + fixed selection switches

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 console.log("Test");
 // API KEY : bead57aed9e1420dbed8834d45b0f26e
-var queryURL =
-  "https://api.spoonacular.com/recipes/76257/information?apiKey=bead57aed9e1420dbed8834d45b0f26e";
+var id =
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-});
+var queryURL =
+  "https://api.spoonacular.com/recipes/search?apiKey=bead57aed9e1420dbed8834d45b0f26e&number=1&query=" + userChoice;
+
+function getRecipeID() {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/" +
+      id +
+      "/information?apiKey=bead57aed9e1420dbed8834d45b0f26e",
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
+getRecipeID();
+
+// Recipe Name, Picture of recipe, Ingredient list, instructions, Time to cook
+// Servings
+// 1 recipe response
+// How to get recipe instructions bullet pointed *
