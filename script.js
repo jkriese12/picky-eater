607292b430865e7f1dd0ac3054f5def14b27167a
Jacob Kriese <jkriese12@gmail.com>
2021-01-04 19:18:08 -0700

    Added if else if it does not give a response

--- script.js
+++ script.js
@@ -2,19 +2,18 @@
 // change
 // change again
 var check = $("#tinySwitch");
-console.log(check.checked);
+
 function getRecipeID() {
   var id = $("input").val().trim();
   var glutenCheck = $("#tinySwitch").is(":checked");
   var veganCheck = $("#tinySwitch2").is(":checked");
   var dairyCheck = $("#tinySwitch3").is(":checked");
-  console.log(glutenCheck, veganCheck, dairyCheck);
+
   var dietChoice = "";
   if (glutenCheck === true && veganCheck === false && dairyCheck === false) {
     var dietChoice = "&diet=gluten-free";
   } else if (glutenCheck === true && veganCheck === true && dairyCheck === false) {
     var dietChoice = "&diet=vegan&intolerances=gluten";
-    alert("ok");
   } else if (glutenCheck === true && veganCheck === true && dairyCheck === true) {
     var dietChoice = "&diet=gluten-free,vegan&intolerances=dairy";
   } else if (glutenCheck === true && veganCheck === false && dairyCheck === true) {
@@ -36,46 +35,49 @@ function getRecipeID() {
       id,
     method: "GET",
   }).then(function (response) {
-    console.log(response.results[0].id);
+    // console.log(response.results[0].id);
     console.log(response);
-    var foodID = response.results[0].id;
-    var urlID =
-      "https://api.spoonacular.com/recipes/" +
-      foodID +
-      "/information?apiKey=d62f94a01ecd492192a2ac5bb4bf78f9";
-    $.ajax({
-      url: urlID,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      console.log(response.sourceUrl);
-      $("#ingredients").text("Recipe Ingredients:");
-      $("#instructions").text("Recipe Instructions:");
-      $("#recipeName").text(response.title);
-      $("#img").attr("src", response.image);
-      var a = $("<a>");
-      a.attr("href", response.sourceUrl);
-      a.attr("target", "blank");
-      a.text(response.sourceUrl);
-      var button = $("<button>");
-      button.text("Save me?");
-      $(".tipsPanel").append(a);
-      $(".tipsPanel").append(button);
+    if (response.results.length > 0) {
+      var foodID = response.results[0].id;
+      var urlID =
+        "https://api.spoonacular.com/recipes/" +
+        foodID +
+        "/information?apiKey=d62f94a01ecd492192a2ac5bb4bf78f9";
+      $.ajax({
+        url: urlID,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        console.log(response.sourceUrl);
+        $("#ingredients").text("Recipe Ingredients:");
+        $("#instructions").text("Recipe Instructions:");
+        $("#recipeName").text(response.title);
+        $("#img").attr("src", response.image);
+        var a = $("<a>");
+        a.attr("href", response.sourceUrl);
+        a.attr("target", "blank");
+        a.text(response.sourceUrl);
+        var button = $("<button>");
+        button.text("Save me?");
+        $(".tipsPanel").append(a);
+        $(".tipsPanel").append(button);
 
-      // var listItems = $("<li>").text(response.extendedIngredients[i].name);
-      for (var i = 0; i < response.extendedIngredients.length; i++) {
-        JSON.stringify(response.extendedIngredients[i].name);
-        var listItem = $("<li>");
-        listItem.text(response.extendedIngredients[i].name);
-        $(".ingredientsList").append(listItem);
-      }
-      for (var i = 0; i < response.analyzedInstructions[0].steps.length; i++) {
-        var instructionItem = $("<li>");
-        JSON.stringify(response.analyzedInstructions[0].steps[i].step);
-        instructionItem.text(response.analyzedInstructions[0].steps[i].step);
-        $(".instructionsList").append(instructionItem);
-      }
-    });
+        // var listItems = $("<li>").text(response.extendedIngredients[i].name);
+        for (var i = 0; i < response.extendedIngredients.length; i++) {
+          JSON.stringify(response.extendedIngredients[i].name);
+          var listItem = $("<li>");
+          listItem.text(response.extendedIngredients[i].name);
+          $(".ingredientsList").append(listItem);
+        }
+        for (var i = 0; i < response.analyzedInstructions[0].steps.length; i++) {
+          var instructionItem = $("<li>");
+          JSON.stringify(response.analyzedInstructions[0].steps[i].step);
+          instructionItem.text(response.analyzedInstructions[0].steps[i].step);
+          $(".instructionsList").append(instructionItem);
+        }
+      });
+    } else {
+    }
   });
 }
 
